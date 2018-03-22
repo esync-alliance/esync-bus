27a1a0331fcda264058211d5503e75d192ec4b6d
Khoa Nguyen <khoa.nguyen@gontekus.com>
2018-03-22 23:08:27 +0700

    Resolved merge conflict

--- qnx_port/build_lib.sh
+++ qnx_port/build_lib.sh
@@ -4,14 +4,14 @@ portdir=`pwd`
 
 build_lib () {
 
-	cd ${portdir}
+	cd ${portdir}/../
 
-	export LIBXL4BUS_PORT=.
+	export LIBXL4BUS_PORT=qnx_port
 
 	if [ ! -d build ];then
 		mkdir build
 		cd build
-		cmake -DCMAKE_TOOLCHAIN_FILE=qnx.cmake -DBUILD_SHARED=ON -DBUILD_STATIC=OFF -DXL4_SUPPORT_IPV6=0 .. || return 1
+		cmake -DCMAKE_TOOLCHAIN_FILE=../qnx_port/qnx.cmake -DBUILD_SHARED=ON -DBUILD_STATIC=OFF -DXL4_SUPPORT_IPV6=0 .. || return 1
 		make || return 1
 	fi
 
@@ -19,14 +19,14 @@ build_lib () {
 }
 
 if [ "$1" = 'clean' ] ; then
-	cd ${portdir}
-	rm -rf build
-	rm *.h
+	rm -rf ${portdir}/../build
+	rm -f ${portdir}/*.h
 	exit 0
 else
 	build_lib
 	if [ $? -eq 1 ]; then
-		rm -rf ${portdir}/build
+		rm -rf ${portdir}/../build
+		rm -f ${portdir}/*.h
 		exit 1
 	fi
 fi
