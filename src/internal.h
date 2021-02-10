bfe9bbb37144e583fa78ff0034d24b9f4b815aad
Pawel S. Veselov <pawel.veselov@excelfore.com>
2021-02-09 18:53:29 -0800

    ESYNC-5224 Don't use union for holding frame length

--- src/internal.h
+++ src/internal.h
@@ -186,16 +186,14 @@ typedef struct frame {
 
     size_t total_read;
     uint8_t byte0;
-    union {
-        uint32_t frame_len;
-        uint8_t len_bytes[3];
-    };
+    uint8_t len_bytes[3];
 
     xl4bus_buf_t data;
 
     uint32_t crc;
 
     int len_converted;
+    uint32_t frame_len;
     uint64_t expires_at;
 
     int hashed;
